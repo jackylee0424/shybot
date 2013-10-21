b4ebb8d0a2a04a4a876df786a9fe750f6669102e
Jackie Lee <jackylee0424@gmail.com>
2013-10-21 14:26:59 -0700

    make xcode4 compatible storyboard
    Former-commit-id: 7f31edb6c7dedb01b041ea8f9786cf16d62c3f34 [formerly 0d83eebc7b73a7119e69eb40a3ecc13c767bd464] [formerly 5e5a97ee204a0b12fbea3d7b518eec4fe4ade718 [formerly 3e334a39ca4169bb485b4ef8d46bd226a8c67cab]]
    Former-commit-id: 09f670d5e95e41a514c58fdc540cdb81b65a53aa
    Former-commit-id: fcb4505373ef8f6fae1b13f414c0e83e0cb6a032

--- Shybot/Shybot/FaceDetector.mm
+++ Shybot/Shybot/FaceDetector.mm
@@ -55,7 +55,7 @@ const int kHaarOptions =  CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH;
 - (std::vector<cv::Rect>)facesFromImage:(cv::Mat&)image
 {
     std::vector<cv::Rect> faces;
-    _faceCascade.detectMultiScale(image, faces, 1.1, 2, kHaarOptions, cv::Size(50, 50));
+    _faceCascade.detectMultiScale(image, faces, 1.1, 2, kHaarOptions, cv::Size(35, 35));
     
     return faces;
 }
